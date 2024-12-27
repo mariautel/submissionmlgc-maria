4f5ca0e43a775e8f11835811943dfc584755a1a7
mariautel <mariachristinautel@gmail.com>
2024-12-27 08:56:01 +0700

    upload file

---
+++ src/server/routes.js
@@ -0,0 +1,22 @@
+const { postPredictHandler, postPredictHistoriesHandler } = require('../server/handler');
+ 
+const routes = [
+  {
+    path: '/predict',
+    method: 'POST',
+    handler: postPredictHandler,
+    options: {
+      payload: {
+        allow: 'multipart/form-data',
+        multipart: true
+      }
+    }
+  },
+  {
+    path: '/predict/histories',
+    method: 'GET',
+    handler: postPredictHistoriesHandler,
+  }
+]
+ 
+module.exports = routes;
